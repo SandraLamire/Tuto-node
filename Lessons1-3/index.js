f40241a77fb1d3a99ace7d78ab215a4bb48b8a5e
Sandra <sandra.lamire.sl@gmail.com>
2023-11-14 11:21:33 +0100

    first commit

---
+++ Lessons1-3/index.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+// readFileSync = lire le contenu d'un fichier
+// writeFileSync = écrire un fichier avec un contenu
+// Lancer le programme dans la cosole = node index.js
+
+// fs.writeFileSync("test.txt", "Mon premier fichier")
+
+// cls pour effacer la console
+fs.writeFileSync("test.txt", "Mon second fichier");
+console.log("Le fichier test.txt a été crée!");
+
+// const fileContent = fs.readFileSync("test.txt");
+// console.log(fileContent);
+// renvoie un buffer car encoding non spécifié
+
+const fileContent = fs.readFileSync("test.txt", "utf8");
+console.log(fileContent);
+// Le fichier test.txt a été crée!
+// Mon second fichier
+
+
+
+
