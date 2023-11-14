56249783836d3a4f5071f55475976961e741dca2
Sandra <sandra.lamire.sl@gmail.com>
2023-11-14 16:17:05 +0100

    Asynchrone

---
+++ Lesson8/index.js
@@ -0,0 +1,19 @@
+// changer de dossier dans la console : cd ../Lesson8
+
+console.log("1");
+console.log("2");
+console.log("3");
+// node index.js affiche 1 2 3
+
+// ASYNCHRONICITE grâce à setTimeOut
+
+console.log("1 - Je suis lancé de suite!");
+// Si pas d'argument à passer dans la fonction = parenthèses vides (ou _) avant la =>
+// 3000 = 3 secondes d'attente avant le callback
+setTimeout(() => {
+    console.log("2 - Et après 3 secondes, c'est moi qui suis exécuté!");
+}, 3000)
+console.log("3 - Après, c'est moi!");
+// node index.js affiche 1 3 2
+
+// ASYNCHRONICITE grâce à async/await (vu + tard)
