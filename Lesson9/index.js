444fa7798c92bf22f90b5897b86742ef4a8b157e
Sandra <sandra.lamire.sl@gmail.com>
2023-11-14 17:00:11 +0100

    Ajout DOG_API_KEY

--- Lesson9/index.js
+++ Lesson9/index.js
@@ -2,16 +2,30 @@
 // API Chien: https://thedogapi.com => request = https://api.thedogapi.com/v1/images/search
 // API Chat: https://thecatapi.com => request = https://api.thecatapi.com/v1/images/search
 
-
+require('dotenv').config();
 // npm i postman-request
 const req = require("postman-request");
+const apiKey = process.env.DOG_API_KEY;
 
 // Nécessite une clé API
-// req("https://api.thedogapi.com/v1/images/search", (error, response, body) => {
-//     const dogPicture = JSON.parse(body);
-//     console.log(dogPicture.message);
-//     console.log(dogPicture.status);
-// })
+// npm install dotenv
+// Vérifier si la clé API est définie
+if (!apiKey) {
+    console.error('La clé API pour The Dog API n\'est pas définie.');
+    process.exit(1);
+  }
+  
+  // Utiliser la clé API dans la requête
+  req(`https://api.thedogapi.com/v1/images/search?api_key=${apiKey}`, (error, response, body) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+  
+    const dogPicture = JSON.parse(body);
+    console.log(dogPicture.message);
+    console.log(dogPicture.status);
+  });
 
 
 // Pas de clé API nécessaire
