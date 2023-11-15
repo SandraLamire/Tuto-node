3cbdaf907e813130b84cc5f243872bf03bfbc6b8
Sandra <sandra.lamire.sl@gmail.com>
2023-11-15 09:56:09 +0100

    Correction chemin vers .env

--- Lesson9/index.js
+++ Lesson9/index.js
@@ -1,11 +1,12 @@
 // Utilisation d'API pour récupérer des images de chats et chiens
 // API Chien: https://thedogapi.com => request = https://api.thedogapi.com/v1/images/search
 // API Chat: https://thecatapi.com => request = https://api.thecatapi.com/v1/images/search
+// Run l'appli avec node Lesson9/index.js
 
-require('dotenv').config();
+require('dotenv').config({ path: '../TUTO-NODE/.env' });
 // npm i postman-request
-const req = require("postman-request");
-const apiKey = process.env.DOG_API_KEY;
+const req = require('postman-request');
+const apiKey = process.env.MY_DOG_API_KEY;
 
 // Nécessite une clé API
 // npm install dotenv
@@ -21,16 +22,14 @@ if (!apiKey) {
       console.error(error);
       return;
     }
-  
     const dogPicture = JSON.parse(body);
-    console.log(dogPicture.message);
-    console.log(dogPicture.status);
+    console.log(dogPicture[0].url);
   });
 
 
 // Pas de clé API nécessaire
 req("https://api.thecatapi.com/v1/images/search", (error, response, body) => {
     const catPicture = JSON.parse(body);
-    console.log(catPicture[0].url);
+    console.log("URL image de chat:", catPicture[0].url);
 })
 
